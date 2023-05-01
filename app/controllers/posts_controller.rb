9eb74d695294cd99961896d656c289ff7e7072fc
ogitaniryosuke <gtn.r74@gmail.com>
2023-05-01 18:09:21 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "desc")
   end
 
-  def new
-  end
+
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: index
   end
 end
